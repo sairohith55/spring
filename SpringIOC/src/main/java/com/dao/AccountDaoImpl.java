b869d193368c7765e4162500a54657716e995e59
tsuser <tsuser@tsuser>
2018-01-20 11:24:33 +0530

    2nd

--- SpringIOC/src/main/java/com/dao/AccountDaoImpl.java
+++ SpringIOC/src/main/java/com/dao/AccountDaoImpl.java
@@ -6,42 +6,35 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Required;
 
 import com.beans.Account;
 
 public class AccountDaoImpl implements AccountDao {
 	
+	@Autowired
 	private DataSource datasource;
 	
 	public AccountDaoImpl(){
 		
 	}
 
-	@Required
-	public void setDatasource(DataSource datasource) {
-		this.datasource = datasource;
-	}
-
 	public void set(Account account) {
 	    Connection con = null;
         PreparedStatement pstmt = null;
         try {
-            // Get connection.
             con = datasource.getConnection();
-            // Create statement.
             pstmt = con.prepareStatement("INSERT INTO account VALUES(?,?,?,?)");
-            // Set parameters.
             pstmt.setInt(1, account.getAccno());
             pstmt.setString(2, account.getAccname());
             pstmt.setString(3, account.getAcctype());
             pstmt.setDouble(4, account.getBalance());
-            // Execute statement.
             pstmt.execute();
             System.out.println("Record Inserted successfully");
-        } catch (Exception e) {// Handle Exceptions
+        } catch (Exception e) {
             e.printStackTrace();
-        } finally {// Clean up resources to avoid memory leaks problems.
+        } finally {
             try {
                 pstmt.close();
                 con.close();
--- SpringIOC/src/main/java/com/test/ClientCode.java
+++ SpringIOC/src/main/java/com/test/ClientCode.java
@@ -10,16 +10,14 @@ import com.dao.AccountDaoImpl;
 public class ClientCode {
 
 	public static void main(String[] args) {
-
-	    ApplicationContext application = new ClassPathXmlApplicationContext("applicationContext.xml");
-	    AccountDaoImpl accDao = (AccountDaoImpl)application.getBean("accdao");
-	    
+	    @SuppressWarnings("resource")
+		ApplicationContext application = new ClassPathXmlApplicationContext("applicationContext.xml");
+	    AccountDaoImpl accDao = (AccountDaoImpl)application.getBean("accdao");   
 	    Account account = new Account();
 	    account.setAccno(1);
-	    account.setAccname("imitaz");
+	    account.setAccname("Rohith");
 	    account.setAcctype("savings");
-	    account.setBalance(2000.00);    
+	    account.setBalance(5000.00);    
 	    accDao.set(account);
-
 	}
 }	
